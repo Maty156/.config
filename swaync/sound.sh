e2893b3726aaaf5e7e31c7fa4f154415c1359c12
Matyas Abreham <matyasabreham7@gmail.com>
2026-06-06 20:16:23 +0300

    Update sound script to use new notification sound

--- swaync/sound.sh
+++ swaync/sound.sh
@@ -1,5 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail
+
 if [ "$(swaync-client -D)" = "false" ]; then
-    mpv --no-video --no-terminal ~/.config/swaync/sounds/universfield-new-notification-09-352705.mp3
+    mpv --no-video --no-terminal ~/.config/swaync/sounds/dragon-studio-new-notification-3-398649.mp3
 fi
 
